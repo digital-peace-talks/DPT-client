42a22221848b4359e5772ad0d655624397c9e6a7
REUT <reut@soficoop.com>
2018-12-12 19:00:28 +0200

    fixing routes in router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Router from 'vue-router'
-import Home from './components/Home'
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
+import Home from './components/Home/Home'
+import SignIn from './components/SignIn/SignIn'
+import SignUp from './components/SignUp/SignUp'
 
 export default new Router ({
     routes: [
