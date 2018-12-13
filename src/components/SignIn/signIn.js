d04146debd09066bd3792abc69fd9b0a5bf7f15d
oz <ozfraier@gmail.com>
2018-12-13 08:50:05 +0200

    tiny update, submitting the sign-in form calls a sign-in method that prints the mnemonic

--- src/components/SignIn/signIn.js
+++ src/components/SignIn/signIn.js
@@ -7,49 +7,53 @@ export default {
             /**
              * @type string
              */
-            key: null,
+            mnemonic: null,
             rules: {
                 validateMnemonic:(value) => {
                     const wordCount=value.trim().split(/\s+/).length
                     if(wordCount>12){
-                    return "passphrase too long, must be 12 words"
+                        return "passphrase too long, must be 12 words"
                     }
                     if(wordCount<12){
-                    return "passphrase too short, must be 12 words"
+                        return "passphrase too short, must be 12 words"
                     }
                     if(!bip39.validateMnemonic(value)){
-                    return "passphrase doesn't match"
-                }
+                        return "passphrase doesn't match"
+                    }
+                    return true
                 } 
             }
         }
     },
-            methods: {
-                /**
-                 * Sets this.key according to clipboard text
-                 */
-                pasteFromClipboard() {
-                    navigator.clipboard.readText().then(text => (this.key = text));
-                },
-                    loadFile() {
-                        this.$refs.fileInput.click();
-                    },
-                    /**
-                     * Executed after the user has selected a file
-                     * @param {Event} evt
-                     */
-                    onFilePicked() {
-                        /**
-                         * @type FileList
-                         */
-                        const files = this.$refs.fileInput.files;
-                        if (files[0] !== undefined) {
-                            let reader = new FileReader();
-                            reader.onload = () => {
-                                this.key = reader.result;
-                            };
-                            reader.readAsText(files[0]);
-                        }
-                    }
+    methods: {
+        login(){
+            console.log(this.mnemonic)
+        },
+        /**
+         * Sets this.key according to clipboard text
+         */
+        pasteFromClipboard() {
+            navigator.clipboard.readText().then(text => (this.mnemonic = text));
+        },
+        loadFile() {
+            this.$refs.fileInput.click();
+        },
+        /**
+         * Executed after the user has selected a file
+         * @param {Event} evt
+         */
+        onFilePicked() {
+            /**
+             * @type FileList
+             */
+            const files = this.$refs.fileInput.files;
+            if (files[0] !== undefined) {
+                let reader = new FileReader();
+                reader.onload = () => {
+                    this.mnemonic = reader.result;
+                };
+                reader.readAsText(files[0]);
             }
-    };
+        }
+    }
+};
