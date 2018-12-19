87444e7e5e39ddc68d895371cbd94d04d0d32a19
Aviran Katz <aviran@soficoop.com>
2018-12-19 13:23:54 +0200

    nightwatch now loads from a vue cli plugin

---
+++ tests/e2e/specs/home.test.js
@@ -0,0 +1,26 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+
+// eslint-disable-next-line no-unused-vars
+import { NightwatchBrowser } from "nightwatch";
+
+/**
+ * The main test
+ * @param {NightwatchBrowser} browser 
+ */
+function main(browser) {
+  browser
+    .url(process.env.VUE_DEV_SERVER_URL)
+    .waitForElementVisible('body', 1000)
+    .waitForElementVisible('.v-btn', 1000)
+    .click('.v-btn')
+    .pause(1000)
+    .waitForElementVisible('.headline', 3000)
+    .assert.elementCount('body', 1)
+    .end();
+}
+
+module.exports = {
+  'default e2e tests': main
+}
