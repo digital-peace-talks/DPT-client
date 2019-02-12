47a4714798ed85ba2a4846edd587a08b824857b2
oz <ozfraier@gmail.com>
2019-02-12 14:41:34 +0200

    linting changes

--- src/Store/ChatModule.js
+++ src/Store/ChatModule.js
@@ -1,59 +1,62 @@
-import * as firebase from 'firebase'
+import * as firebase from "firebase";
 
 const ChatModule = {
   state: {
     chats: []
   },
   mutations: {
-    setMessagesEmpty (state) {
-      state.messages = []
+    setMessagesEmpty(state) {
+      state.messages = [];
     },
-    setChats (state, payload) {
-      state.chats = payload
+    setChats(state, payload) {
+      state.chats = payload;
     }
   },
   actions: {
-    sendMessage ({commit}, payload) {
-      let chatID = payload.chatID
+    sendMessage({ commit }, payload) {
+      let chatID = payload.chatID;
       const message = {
         user: payload.username,
         content: payload.content,
         date: payload.date
-      }
-      firebase.firestore().collection('messages').doc(chatID).collection('messages').add(message)
-        .then(
-          (data) => {
-          }
-        )
-        .catch(
-          (error) => {
-            console.log(error)
-          }
-        )
+      };
+      firebase
+        .firestore()
+        .collection("messages")
+        .doc(chatID)
+        .collection("messages")
+        .add(message)
+        .then(data => {})
+        .catch(error => {
+          console.log(error);
+        });
     },
-    loadChats ({commit}) {
-      let chatRef =firebase.firestore().collection('chats')
-      let chats = chatRef//.get()
-      if(chats){
+    loadChats({ commit }) {
+      let chatRef = firebase.firestore().collection("chats");
+      let chats = chatRef; //.get()
+      if (chats) {
         //console.log(chats.get())
-        commit('setChats',[{statement: 'asd',author:"Alice"},{statement: 'bsd',author:"Alice"}])
+        commit("setChats", [
+          { statement: "asd", author: "Alice" },
+          { statement: "bsd", author: "Alice" }
+        ]);
       }
     },
-    createChat ({commit}, payload) {
+    createChat({ commit }, payload) {
       //do something with firestore
       return new Promise((resolve, reject) => {
-        resolve(newPostKey)
-      })
+        resolve(newPostKey);
+      });
     }
   },
   getters: {
-    messages (state) {
-      return state.messages
+    messages(state) {
+      return state.messages;
     },
-    chats (state) {
-      return state.chats
+    chats(state) {
+      return state.chats;
     }
   }
-}
+};
 
-export default ChatModule
+export default ChatModule;
