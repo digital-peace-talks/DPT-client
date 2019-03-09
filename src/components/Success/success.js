e4cf2bf4053fac0febcae2a0d85169afd5467d55
oz <ozfraier@gmail.com>
2019-03-10 00:20:18 +0200

    make Success form design gentler and more condensed, use chat statement for content

--- src/components/Success/success.js
+++ src/components/Success/success.js
@@ -1,8 +1,12 @@
 export default {
   props: {
-    value: Boolean
+    value: Boolean,
+    id: "$route.params.id"
   },
   computed: {
+    statement() {
+      return this.$store.getters.chats[this.$route.params.id].statement;
+    },
     show: {
       get() {
         return this.value;
