c6da06027d29e7bf121ff811ac7864eeb6316505
oz <ozfraier@gmail.com>
2019-03-11 09:35:29 +0200

    Success modal and Crisis modal appearance, slider control and date/time pickers

--- src/components/Crisis/crisis.js
+++ src/components/Crisis/crisis.js
@@ -1,4 +1,41 @@
 export default {
+  data: () => ({
+    time: 1,
+    time_ranges: [
+      "hours",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "days",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+  }),
   props: {
     value: Boolean
   },
@@ -9,6 +46,15 @@ export default {
       },
       set(value) {
         this.$emit("input", value);
+      }
+    }
+  },
+  methods: {
+    time_range(val) {
+      if (val > 23) {
+        return val - 23 + " days";
+      } else {
+        return val + " hours";
       }
     }
   }
